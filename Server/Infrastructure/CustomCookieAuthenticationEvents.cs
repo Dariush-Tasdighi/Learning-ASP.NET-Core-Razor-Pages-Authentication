04af4c1c8894cd1ba35690e32d2d9ad8934f5943
Mr. Dariush Tasdighi <DariushT@GMail.com>
2022-05-07 03:01:08 +0430

    Making some changes!

---
+++ Server/Infrastructure/CustomCookieAuthenticationEvents.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Authentication;
+
+namespace Infrastructure
+{
+	public class CustomCookieAuthenticationEvents :
+		Microsoft.AspNetCore.Authentication.Cookies.CookieAuthenticationEvents
+	{
+		public CustomCookieAuthenticationEvents() : base()
+		{
+		}
+
+		// Four callback points for login and logout are defined:
+
+		public override System.Threading.Tasks.Task SigningIn
+			(Microsoft.AspNetCore.Authentication.Cookies.CookieSigningInContext context)
+		{
+			return base.SigningIn(context);
+		}
+
+		public override System.Threading.Tasks.Task SignedIn
+			(Microsoft.AspNetCore.Authentication.Cookies.CookieSignedInContext context)
+		{
+			return base.SignedIn(context);
+		}
+		public override System.Threading.Tasks.Task SigningOut
+			(Microsoft.AspNetCore.Authentication.Cookies.CookieSigningOutContext context)
+		{
+			return base.SigningOut(context);
+		}
+
+		public override async System.Threading.Tasks.Task ValidatePrincipal
+			(Microsoft.AspNetCore.Authentication.Cookies.CookieValidatePrincipalContext context)
+		{
+			var userPrincipal = context.Principal;
+
+			//// Look for the LastChanged claim.
+			//var lastChanged =
+			//	(from c in userPrincipal.Claims
+			//	 where c.Type == "LastChanged"
+			//	 select c.Value).FirstOrDefault();
+
+			if (1 == 2)
+			{
+				context.RejectPrincipal();
+
+				// SignOutAsync () -> using Microsoft.AspNetCore.Authentication;
+				await context.HttpContext.SignOutAsync
+					(Microsoft.AspNetCore.Authentication.Cookies
+					.CookieAuthenticationDefaults.AuthenticationScheme);
+			}
+		}
+
+		// Another 4 callback points for redirection
+
+		public override System.Threading.Tasks.Task RedirectToLogin
+			(Microsoft.AspNetCore.Authentication.RedirectContext
+			<Microsoft.AspNetCore.Authentication.Cookies.CookieAuthenticationOptions> context)
+		{
+			return base.RedirectToLogin(context);
+		}
+
+		public override System.Threading.Tasks.Task RedirectToReturnUrl
+			(Microsoft.AspNetCore.Authentication.RedirectContext
+			<Microsoft.AspNetCore.Authentication.Cookies.CookieAuthenticationOptions> context)
+		{
+			return base.RedirectToReturnUrl(context);
+		}
+
+		public override System.Threading.Tasks.Task RedirectToAccessDenied
+			(Microsoft.AspNetCore.Authentication.RedirectContext
+			<Microsoft.AspNetCore.Authentication.Cookies.CookieAuthenticationOptions> context)
+		{
+			return base.RedirectToAccessDenied(context);
+		}
+
+		public override System.Threading.Tasks.Task RedirectToLogout
+			(Microsoft.AspNetCore.Authentication.RedirectContext
+			<Microsoft.AspNetCore.Authentication.Cookies.CookieAuthenticationOptions> context)
+		{
+			return base.RedirectToLogout(context);
+		}
+	}
+}
--- Server/Program.cs
+++ Server/Program.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿// **************************************************
+using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
+// **************************************************
 
 // **************************************************
 var webApplicationOptions =
@@ -20,31 +22,47 @@ var builder =
 // **************************************************
 // Step (2)
 // **************************************************
+// https://docs.microsoft.com/en-us/aspnet/core/security/authentication/cookie?view=aspnetcore-6.0
+// https://www.red-gate.com/simple-talk/development/dotnet-development/using-auth-cookies-in-asp-net-core/
+// https://programmer.ink/think/understand-asp-net-core-cookie-authentication.html
+// https://github.com/dotnet/AspNetCore.Docs/blob/main/aspnetcore/security/authentication/cookie.mdhttps://github.com/dotnet/AspNetCore.Docs/blob/main/aspnetcore/security/authentication/cookie.md
+
+// https://driesdeboosere.dev/blog/how-to-add-cookie-consent-in-aspnet-core-50/https://driesdeboosere.dev/blog/how-to-add-cookie-consent-in-aspnet-core-50/
+// **************************************************
+// Replace 'Googooli' to:
+//Microsoft.AspNetCore.Authentication.Cookies.CookieAuthenticationDefaults.AuthenticationScheme
+// **************************************************
+builder.Services.AddScoped
+	<Infrastructure.CustomCookieAuthenticationEvents>();
+
 builder.Services
 	.AddAuthentication(defaultScheme: "Googooli")
 	.AddCookie(authenticationScheme: "Googooli", options =>
 	{
 		options.LoginPath = "/Account/Login";
+		options.ReturnUrlParameter = "ReturnUrl";
+
 		options.LogoutPath = "/Account/Logout";
 		options.AccessDeniedPath = "/Account/AccessDenied";
 
-		//options.Cookie.Path;
-		//options.Cookie.Domain;
-		//options.Cookie.MaxAge;
-		//options.Cookie.HttpOnly;
-		//options.Cookie.SameSite;
-		//options.Cookie.Expiration;
-		//options.Cookie.IsEssential;
-		//options.Cookie.SecurePolicy;
-		options.Cookie.Name = "Googooli";
-
 		options.ClaimsIssuer = "DTAT";
 
-		//options.SessionStore
-		//options.ExpireTimeSpan
-		//options.TicketDataFormat
-		options.SlidingExpiration = true;
-		options.ReturnUrlParameter = "ReturnUrl";
+		//options.Cookie.Domain;
+		//options.Cookie.MaxAge;
+		//options.Cookie.Expiration;
+		options.Cookie.Path = "/";
+		options.Cookie.Name = "Googooli";
+		options.Cookie.HttpOnly = true; // Default: 'true'
+		options.Cookie.IsEssential = true; // Default: 'true'
+		options.Cookie.SameSite =
+			Microsoft.AspNetCore.Http.SameSiteMode.Lax; // Default: 'Lax'
+		options.Cookie.SecurePolicy =
+			Microsoft.AspNetCore.Http.CookieSecurePolicy.Always;
+
+		options.ExpireTimeSpan =
+			System.TimeSpan.FromMinutes(value: 1); // Default: 14 Days
+
+		options.SlidingExpiration = true; // Default: 'true'
 
 		//options.ForwardSignIn
 		//options.ForwardForbid
@@ -54,17 +72,33 @@ builder.Services
 		//options.ForwardAuthenticate
 		//options.ForwardDefaultSelector
 
+		//options.SessionStore
 		//options.CookieManager
+		//options.TicketDataFormat
 		//options.DataProtectionProvider
 
-		//options.Events
-		//options.EventsType
+		options.EventsType =
+			typeof(Infrastructure.CustomCookieAuthenticationEvents);
+
+		//options.Events.OnCheckSlidingExpiration = (context) =>
+		//{
+		//	return System.Threading.Tasks.Task.CompletedTask;
+		//};
 
 		options.Validate();
 	});
 
 // Note: Does not work!
 // We should activate Step (3)
+// **************************************************
+
+// **************************************************
+builder.Services.AddRouting(options =>
+{
+	options.LowercaseUrls = true;
+	options.LowercaseQueryStrings = true;
+	//options.SuppressCheckForUnhandledSecurityMetadata = false;
+});
 // **************************************************
 
 // **************************************************
@@ -90,6 +124,42 @@ app.UseHttpsRedirection();
 // **************************************************
 // UseStaticFiles() -> using Microsoft.AspNetCore.Builder;
 app.UseStaticFiles();
+// **************************************************
+
+// **************************************************
+//var cookiePolicyOptions =
+//	new Microsoft.AspNetCore.Builder.CookiePolicyOptions
+//	{
+//		MinimumSameSitePolicy =
+//			Microsoft.AspNetCore.Http.SameSiteMode.Strict,
+
+//		HttpOnly =
+//			Microsoft.AspNetCore.CookiePolicy.HttpOnlyPolicy.Always,
+
+//		OnAppendCookie = (context) =>
+//		{
+//		},
+
+//		OnDeleteCookie = (context) =>
+//		{
+
+//		},
+
+//		//ConsentCookie
+
+//		CheckConsentNeeded = context => true,
+
+//		Secure =
+//			Microsoft.AspNetCore.Http.CookieSecurePolicy.Always,
+
+//		//Secure =
+//		//app.Environment.IsDevelopment()
+//		//? Microsoft.AspNetCore.Http.CookieSecurePolicy.None:
+//		//Microsoft.AspNetCore.Http.CookieSecurePolicy.Always
+//	};
+
+//// UseCookiePolicy() -> using Microsoft.AspNetCore.Builder;
+//app.UseCookiePolicy(options: cookiePolicyOptions);
 // **************************************************
 
 // **************************************************
